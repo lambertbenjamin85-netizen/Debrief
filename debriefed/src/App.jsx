f707b8f2ea7845ddb09eca540de37d212b1fb366
lambertbenjamin85-netizen <lambertbenjamin85@gmail.com>
2025-12-19 18:22:44 -0500

    Delete debriefed directory

--- debriefed/src/App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ fontFamily: "system-ui", padding: "2rem" }}>
-      <h1>Debriefed ✈️</h1>
-      <p>Your app is deployed successfully.</p>
-      <p>This is the foundation. We’ll add features next.</p>
-    </div>
-  );
-}
