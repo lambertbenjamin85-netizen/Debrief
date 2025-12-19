7a5cd012b0e61aaf4077ee4d19a5e65f78907f6b
lambertbenjamin85-netizen <lambertbenjamin85@gmail.com>
2025-12-19 18:19:49 -0500

    Add files via upload

---
+++ debriefed/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: "system-ui", padding: "2rem" }}>
+      <h1>Debriefed ✈️</h1>
+      <p>Your app is deployed successfully.</p>
+      <p>This is the foundation. We’ll add features next.</p>
+    </div>
+  );
+}
